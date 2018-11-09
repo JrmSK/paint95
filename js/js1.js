f35918ccb5d0350e86eb51310d88b4c0d2e51609
JrmSK <jeremy.skornik@gmail.com>
2018-11-09 04:14:06 +0200

    Added modal + its style, I also implemented ability to select a color, that will be highlighted. Previous selected will be un-highlighted.

--- js/js1.js
+++ js/js1.js
@@ -1,49 +1,59 @@
 var Paint = {};
+Paint.colors = ['black', 'red', 'green', 'blue', 'brown', 'white'];      // if you want to add colors, add it here in the array before white (eraser);
+Paint.selectedColor = "";
 
-Paint.start = function() {
+Paint.start = function () {
     Paint.bindMenuActions();
     Paint.generateDynamicColors();
 };
 
-Paint.bindMenuActions = function() {
+Paint.bindMenuActions = function () {
+    var buttonNew = document.getElementById("new");
+    buttonNew.addEventListener("click", Paint.new);
     var buttonSave = document.getElementById("save");
     buttonSave.addEventListener("click", Paint.save);
     var buttonLoad = document.getElementById("load");
     buttonLoad.addEventListener("click", Paint.load);
-    var buttonNew = document.getElementById("new");
-    buttonNew.addEventListener("click", Paint.new);
+
 };
 
-Paint.save = function() {
+Paint.save = function () {
     alert('save');                  // TODELETE
 };
 
-Paint.load = function() {
+Paint.load = function () {
     alert('load');                 // TODELETE
 };
 
-Paint.new = function() {
+Paint.new = function () {
     alert('new');                 // TODELETE
 };
 
-Paint.colors = ['black','red','green','blue','brown','white'];
 
-Paint.generateDynamicColors = function() {
+
+Paint.generateDynamicColors = function () {
     var colorsHolder = document.getElementById("colors-menu");
-    for (var i=0; i<Paint.colors.length;i++) {
+    for (var i = 0; i < Paint.colors.length; i++) {
         var buttonItem = document.createElement('li');
         var newButton = document.createElement('button');
         newButton.style.backgroundColor = Paint.colors[i];
         newButton.style.borderRadius = "50%";
         newButton.style.height = "40px";
         newButton.style.width = "40px";
-        newButton.className = Paint.colors[i];
+        newButton.className = "cover-bg colors-btn";
+        newButton.id = Paint.colors[i];
         buttonItem.appendChild(newButton);
         colorsHolder.appendChild(buttonItem);
+        newButton.addEventListener("click", function(e) {
+            var clickedColor = this;
+            Paint.selectedColor = clickedColor.id;
+            allColorsButtons = document.getElementsByClassName("colors-btn")
+            for (var j = 0; j < Paint.colors.length; j++) {
+                allColorsButtons[j].classList.remove("selected");
+            }
+            clickedColor.classList.add("selected");
+        })
+
     }
 }
-
-
-
-
 Paint.start();
