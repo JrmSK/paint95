318ee31fcff58a09740536b865f2c537980904f1
JrmSK <jeremy.skornik@gmail.com>
2018-11-09 16:48:57 +0200

    Draw function is deployed and working! colors, size are deployed

--- js/js1.js
+++ js/js1.js
@@ -1,10 +1,15 @@
 var Paint = {};
 Paint.colors = ['black', 'red', 'green', 'blue', 'brown', 'white'];      // if you want to add colors, add it here in the array before white (eraser);
 Paint.selectedColor = "";
+Paint.nameOfPainting = "";
+Paint.selectedSize = 5;
 
 Paint.start = function () {
     Paint.bindMenuActions();
     Paint.generateDynamicColors();
+    Paint.generateDynamicSizes();
+    Paint.showModal();
+    Paint.bindCanvasClick();
 };
 
 Paint.bindMenuActions = function () {
@@ -17,6 +22,18 @@ Paint.bindMenuActions = function () {
 
 };
 
+Paint.showModal = function () {
+    var modalWrapper = document.getElementById("modal-wrapper");
+    var nameOfPainting = document.getElementById("painting-name");
+    var validate = document.getElementById("validate-btn");
+    modalWrapper.style.display = "block";
+    validate.addEventListener("click", function () {
+        Paint.nameOfPainting = nameOfPainting.value;
+        modalWrapper.style.display = "none";
+    })
+
+}
+
 Paint.save = function () {
     alert('save');                  // TODELETE
 };
@@ -26,10 +43,33 @@ Paint.load = function () {
 };
 
 Paint.new = function () {
-    alert('new');                 // TODELETE
+    Paint.showModal();
 };
 
+Paint.draw = function (e) {
+    var canvas = document.getElementById("canvas");
+    Paint.pixel = document.createElement("div");
+    canvas.appendChild(Paint.pixel);
+    Paint.pixel.style.position = "absolute";
+    Paint.pixel.style.top = e.clientY - canvas.offsetTop + "px";
+    Paint.pixel.style.left = e.clientX - canvas.offsetLeft + "px";
+    Paint.pixel.style.backgroundColor = Paint.selectedColor;
+    Paint.pixel.style.height = `${Paint.selectedSize}px`;
+    Paint.pixel.style.width = `${Paint.selectedSize}px`;
+    Paint.pixel.style.borderRadius = "30%";
 
+}
+
+Paint.bindCanvasClick = function () {
+    var canvas = document.getElementById("canvas");
+    canvas.addEventListener("click", Paint.draw);
+    canvas.addEventListener("mousedown", function () {
+        canvas.addEventListener("mousemove", Paint.draw);
+    });
+    document.addEventListener("click", function () {
+        canvas.removeEventListener("mousemove", Paint.draw)
+    });
+}
 
 Paint.generateDynamicColors = function () {
     var colorsHolder = document.getElementById("colors-menu");
@@ -44,7 +84,7 @@ Paint.generateDynamicColors = function () {
         newButton.id = Paint.colors[i];
         buttonItem.appendChild(newButton);
         colorsHolder.appendChild(buttonItem);
-        newButton.addEventListener("click", function(e) {
+        newButton.addEventListener("click", function (e) {
             var clickedColor = this;
             Paint.selectedColor = clickedColor.id;
             allColorsButtons = document.getElementsByClassName("colors-btn")
@@ -56,4 +96,16 @@ Paint.generateDynamicColors = function () {
 
     }
 }
+
+Paint.generateDynamicSizes = function () {
+    var sizeHolder = document.getElementById("selected-size");
+    var displaySize = document.getElementById("display-size");
+    displaySize.innerHTML = Paint.selectedSize;
+    sizeHolder.oninput = function () {
+        displaySize.innerHTML = sizeHolder.value;
+        Paint.selectedSize = sizeHolder.value;
+    }
+
+}
+
 Paint.start();
